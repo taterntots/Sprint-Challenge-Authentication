22cb58828e6cebbb55029dd62441ab8604866d80
taterntots <matthew.a.weidner@gmail.com>
2020-01-31 11:34:40 -0500

    both login and registration now produce jwt tokens thanks to a newly created function

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,12 +11,14 @@ router.post('/register', (req, res) => {
 
   Users.add(credentials)
     .then(savedUser => {
-      res.status(201).json(savedUser);
+      const token = generateToken(savedUser); //generates a token upon registration
+
+      res.status(201).json({ savedUser, token });
     })
     .catch(error => {
       res.status(500).json(error);
     })
-});
+})
 
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
@@ -25,7 +27,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bc.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Logged in! Welcome ${user.username}!` }); //attaches token as part of the response
+        const token = generateToken(user); //generates a token upon login
+
+        res.status(200).json({ message: `Logged in! Welcome ${user.username}!`, token }); //attaches token as part of the response
       } else {
         res.status(401).json({ message: 'Invalid Credentials. You shall not pass!' });
       }
@@ -33,6 +37,19 @@ router.post('/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     })
-});
+})
+
+function generateToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
