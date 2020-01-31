1b20145d0729a8ec4fd5bb9f751352f772d3bb5e
taterntots <matthew.a.weidner@gmail.com>
2020-01-31 11:23:15 -0500

    successfully registered a user with a hashed password. no tokens yet

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const bc = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+const Users = require('../helpers/users-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let credentials = req.body;
+  const hash = bc.hashSync(credentials.password, 8); //hashes the password
+  credentials.password = hash;
+
+  Users.add(credentials)
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
 });
 
 router.post('/login', (req, res) => {
